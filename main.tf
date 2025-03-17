9f2d1a4b4bc97a54a9b267a380a8fd23db7067a3
Matt Wynne <matt@mattwynne.net>
2025-03-16 18:15:57 -0700

    fix: Update VM clone ID in main.tf to use correct value

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ provider "proxmox" {
 resource "proxmox_vm_qemu" "vm" {
   name        = "terraform-vm"
   target_node = var.pm_target_node
-  clone       = "your-vm-id"
+  clone       = "100"
 
   network {
     model  = "virtio"
