f62c4a18854224be5f88792561276e17a59b6dff
Matt Wynne <matt@mattwynne.net>
2025-03-19 20:01:19 -0700

    More attempts to get it to work, ip address won't work

--- main.tf
+++ main.tf
@@ -2,76 +2,119 @@ terraform {
   required_providers {
     proxmox = {
       source = "bpg/proxmox"
+      version = "0.73.1"
     }
   }
 }
 
-locals {
-  proxmox_host = "hub"
-}
-
 provider "proxmox" {
-  endpoint = "https://192.168.1.57:8006/api2/json"
+  endpoint = "https://hub.local:8006/"
   insecure = true
-  username = "terraform-prov@pve" 
+  tmp_dir  = "/var/tmp"
+
+  ssh {
+    # agent = true
+    private_key = file("~/.ssh/hub.local")
+    # TODO: uncomment and configure if using api_token instead of password
+    # username = "root"
+  }
 }
 
-resource "proxmox_virtual_environment_container" "container" {
-  node_name   = local.proxmox_host
-  vm_id        = 100
+resource "proxmox_virtual_environment_container" "ubuntu_container" {
+  description = "Managed by Terraform"
+
+  node_name = "hub"
+  vm_id     = 101
+
+  console {
+    enabled   = true
+    tty_count = 2
+    type      = "tty"
+  }
+
+  disk {
+    datastore_id = "local"
+    size         = 8
+  }
 
   initialization {
-    hostname = "test1"
+    hostname = "test3"
 
     ip_config {
       ipv4 {
         address = "dhcp"
       }
+      ipv6 {
+        address = "dhcp"
+      }
+    }
+
+    dns {
+      domain = " "
     }
 
     user_account {
       keys = [
-        file("~/.ssh/hub.local.pub")
+        trimspace(tls_private_key.ubuntu_container_key.public_key_openssh)
       ]
+      password = random_password.ubuntu_container_password.result
     }
   }
 
   network_interface {
-    name = "veth0"
+    name = "eth0"
   }
 
   operating_system {
-    # template_file_id = proxmox_virtual_environment_download_file.latest_ubuntu_22_jammy_lxc_img.id
+    template_file_id = proxmox_virtual_environment_download_file.latest_ubuntu_24.id
     # Or you can use a volume ID, as obtained from a "pvesm list <storage>"
-    template_file_id = "local:vztmpl/ubuntu-24.04-standard_24.04-2_amd64.tar.zst"
+    # template_file_id = "local:vztmpl/jammy-server-cloudimg-amd64.tar.gz"
     type             = "ubuntu"
   }
 
-  # memory      = 512
-  # cpu {
-  #   cores = 1
-  # }
-  # onboot      = true
-  # unprivileged = true
-
-  # ssh_keys = file("~/.ssh/hub.local.pub")
-
-  # disk {
-  #   storage = "local-lvm"
-  #   size    = 8
+  # mount_point {
+  #   # volume mount, a new volume will be created by PVE
+  #   volume = "local-lvm"
+  #   size   = "10G"
+  #   path   = "/mnt/volume"
   # }
 
-  connection {
-    type        = "ssh"
-    user        = "root"
-    host        = "${local.proxmox_host}.local"
-    private_key = file("~/.ssh/hub.local")
+  startup {
+    order      = "3"
+    up_delay   = "60"
+    down_delay = "60"
   }
+}
 
-  provisioner "remote-exec" {
-    inline = [
-      "lxc-attach -n ${self.id} -- bash -c 'echo \"$(cat test1-init.sh)\" > /root/init.sh'",
-      "lxc-attach -n ${self.id} -- bash /root/init.sh"
-    ]
-  }
+resource "proxmox_virtual_environment_download_file" "latest_ubuntu_24" {
+  content_type = "vztmpl"
+  datastore_id = "local"
+  node_name    = "hub"
+  # http://download.proxmox.com/images/system/
+  url          = "http://download.proxmox.com/images/system/ubuntu-24.10-standard_24.10-1_amd64.tar.zst"
+}
+
+resource "random_password" "ubuntu_container_password" {
+  length           = 16
+  override_special = "_%@"
+  special          = true
+}
+
+resource "tls_private_key" "ubuntu_container_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+output "ubuntu_container_password" {
+  value     = random_password.ubuntu_container_password.result
+  sensitive = true
+}
+
+output "ubuntu_container_private_key" {
+  value     = tls_private_key.ubuntu_container_key.private_key_pem
+  sensitive = true
+}
+
+output "ubuntu_container_public_key" {
+  value = tls_private_key.ubuntu_container_key.public_key_openssh
 }
--- outputs.tf
+++
@@ -1,4 +0,0 @@
-output "lxc_id" {
-  description = "The ID of the created LXC container"
-  value       = proxmox_virtual_environment_container.container.id
-}
