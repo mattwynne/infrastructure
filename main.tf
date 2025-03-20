432db347713beb12b7e82dc81733528fdaa70424
Matt Wynne <matt@mattwynne.net>
2025-03-19 17:53:44 -0700

    Getting towards a working configuration again

--- main.tf
+++ main.tf
@@ -13,31 +13,53 @@ locals {
 provider "proxmox" {
   endpoint = "https://192.168.1.57:8006/api2/json"
   insecure = true
+  username = "terraform-prov@pve" 
 }
 
-# See https://registry.terraform.io/providers/Telmate/proxmox/latest/docs/resources/lxc
 resource "proxmox_virtual_environment_container" "container" {
   node_name   = local.proxmox_host
-  vmid        = 100
-  template    = "local:vztmpl/ubuntu-24.04-standard_24.04-2_amd64.tar.zst"
-  memory      = 512
-  cpu {
-    cores = 1
-  }
-  onboot      = true
-  unprivileged = true
+  vm_id        = 100
 
-  ssh_keys = file("~/.ssh/hub.local.pub")
+  initialization {
+    hostname = "test1"
 
-  disk {
-    storage = "local-lvm"
-    size    = 8
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+
+    user_account {
+      keys = [
+        file("~/.ssh/hub.local.pub")
+      ]
+    }
   }
 
-  network {
-    model   = "virtio"
-    bridge  = "vmbr0"
+  network_interface {
+    name = "veth0"
   }
+
+  operating_system {
+    # template_file_id = proxmox_virtual_environment_download_file.latest_ubuntu_22_jammy_lxc_img.id
+    # Or you can use a volume ID, as obtained from a "pvesm list <storage>"
+    template_file_id = "local:vztmpl/ubuntu-24.04-standard_24.04-2_amd64.tar.zst"
+    type             = "ubuntu"
+  }
+
+  # memory      = 512
+  # cpu {
+  #   cores = 1
+  # }
+  # onboot      = true
+  # unprivileged = true
+
+  # ssh_keys = file("~/.ssh/hub.local.pub")
+
+  # disk {
+  #   storage = "local-lvm"
+  #   size    = 8
+  # }
 
   connection {
     type        = "ssh"
