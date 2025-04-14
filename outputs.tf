0c0579a881b67275e66951ed2aa2353d8d150392
Matt Wynne <matt@mattwynne.net>
2025-04-13 18:51:22 -0700

    feat: Add output for sensitive container IDs in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,11 @@ output "containers" {
   value = local.containers
 }
 
+output "ids" {
+  value     = proxmox_virtual_environment_container.container[each.key]
+  sensitive = true
+}
+
 output "container_private_key" {
   value     = tls_private_key.container_key.private_key_pem
   sensitive = true
